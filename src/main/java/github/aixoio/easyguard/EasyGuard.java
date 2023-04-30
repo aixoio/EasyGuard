4e157b674fbd703e90abac4c8f8fef8cd71d60d7
aixoio <79969530+aixoio@users.noreply.github.com>
2023-04-30 19:18:25 +1000

    Add getPlugin method

--- src/main/java/github/aixoio/easyguard/EasyGuard.java
+++ src/main/java/github/aixoio/easyguard/EasyGuard.java
@@ -8,10 +8,15 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class EasyGuard extends JavaPlugin {
 
+
+    private static EasyGuard PLUGIN;
+
     @Override
     public void onEnable() {
 
         this.getLogger().info("Starting...");
+
+        EasyGuard.PLUGIN = this;
 
         this.getLogger().info("Started");
 
@@ -51,6 +56,12 @@ public final class EasyGuard extends JavaPlugin {
         }
 
         return (WorldEditPlugin) plugin;
+
+    }
+
+    public static EasyGuard getPlugin() {
+
+        return EasyGuard.PLUGIN;
 
     }
 
