56098d8ee3eaf2d76396b93eb8b52b268eb94b6b
aixoio <79969530+aixoio@users.noreply.github.com>
2023-04-30 18:02:05 +1000

    Init

---
+++ src/main/java/github/aixoio/easyguard/EasyGuard.java
@@ -0,0 +1,17 @@
+package github.aixoio.easyguard;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class EasyGuard extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
