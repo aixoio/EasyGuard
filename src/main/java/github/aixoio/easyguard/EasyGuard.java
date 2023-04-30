47afc5427c593f427a2bba9de74e19c9f3fc4a4d
aixoio <79969530+aixoio@users.noreply.github.com>
2023-04-30 18:34:52 +1000

    Add getWorldEdit method

--- src/main/java/github/aixoio/easyguard/EasyGuard.java
+++ src/main/java/github/aixoio/easyguard/EasyGuard.java
@@ -1,5 +1,6 @@
 package github.aixoio.easyguard;
 
+import com.sk89q.worldedit.bukkit.WorldEditPlugin;
 import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.Plugin;
@@ -36,6 +37,20 @@ public final class EasyGuard extends JavaPlugin {
         }
 
         return (WorldGuardPlugin) plugin;
+
+    }
+
+    public static WorldEditPlugin getWorldEdit() {
+
+        Plugin plugin = Bukkit.getServer().getPluginManager().getPlugin("WorldEdit");
+
+        if (plugin == null || !(plugin instanceof WorldEditPlugin)) {
+
+            return null;
+
+        }
+
+        return (WorldEditPlugin) plugin;
 
     }
 
