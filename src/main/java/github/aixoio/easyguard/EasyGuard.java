eb0e99ad153d6b17359050d41662e327aa9be478
aixoio <79969530+aixoio@users.noreply.github.com>
2023-04-30 18:33:58 +1000

    Add getWorldGuard method

--- src/main/java/github/aixoio/easyguard/EasyGuard.java
+++ src/main/java/github/aixoio/easyguard/EasyGuard.java
@@ -1,5 +1,8 @@
 package github.aixoio.easyguard;
 
+import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class EasyGuard extends JavaPlugin {
@@ -21,4 +24,19 @@ public final class EasyGuard extends JavaPlugin {
         this.getLogger().info("Stopped");
 
     }
+
+    public static WorldGuardPlugin getWorldGuard() {
+
+        Plugin plugin = Bukkit.getServer().getPluginManager().getPlugin("WorldGuard");
+
+        if (plugin == null || !(plugin instanceof WorldGuardPlugin)) {
+
+            return null;
+
+        }
+
+        return (WorldGuardPlugin) plugin;
+
+    }
+
 }
