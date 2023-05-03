98d19cbb52157112448e89083ddd1b5240b4a65a
aixoio <79969530+aixoio@users.noreply.github.com>
2023-05-03 19:11:15 +1000

    Bug fix

--- src/main/java/github/aixoio/easyguard/commands/TrustCommand.java
+++ src/main/java/github/aixoio/easyguard/commands/TrustCommand.java
@@ -144,6 +144,13 @@ public class TrustCommand implements CommandExecutor {
 
                     owners.removePlayer(targetPlayer);
 
+                    if (owners.size() == 0 || !owners.contains(targetPlayer)) {
+
+                        sender.sendMessage(ChatColor.RED + "To delete this claim use /remove-claim " + location);
+                        return true;
+
+                    }
+
                     region.setOwners(owners);
 
                     sender.sendMessage(ChatColor.GREEN + targetPlayerUsername + " was removed from " + region.getId());
