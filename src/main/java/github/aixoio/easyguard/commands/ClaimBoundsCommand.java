6867e51038244be34bc78de40e5f89b4ed1e9480
aixoio <79969530+aixoio@users.noreply.github.com>
2023-06-03 10:17:18 +1000

    Add block line options for /claim-bounds

--- src/main/java/github/aixoio/easyguard/commands/ClaimBoundsCommand.java
+++ src/main/java/github/aixoio/easyguard/commands/ClaimBoundsCommand.java
@@ -7,10 +7,12 @@ import com.sk89q.worldguard.protection.ApplicableRegionSet;
 import com.sk89q.worldguard.protection.regions.ProtectedRegion;
 import github.aixoio.easyguard.EasyGuard;
 import org.bukkit.*;
+import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.util.Vector;
 
 public class ClaimBoundsCommand implements CommandExecutor {
@@ -55,9 +57,16 @@ public class ClaimBoundsCommand implements CommandExecutor {
 
             World world = player.getWorld();
 
-            Particle.DustOptions dust = new Particle.DustOptions(Color.YELLOW, (float) EasyGuard.getPlugin().getConfig().getDouble("POINT_SIZE"));
+            Material corrorBlockMaterial = Material.getMaterial(EasyGuard.getPlugin().getConfig().getString("CORRONR_BLOCK"));
+            Material lineBlockMaterial = Material.getMaterial(EasyGuard.getPlugin().getConfig().getString("LINE_BLOCk"));
 
-            // NOTE: Draw points - START DO NOT CHANGE
+            boolean withcorrorblocks = true;
+            boolean withlineblocks = true;
+
+            if (corrorBlockMaterial == null || corrorBlockMaterial == Material.AIR) withcorrorblocks = false;
+            if (lineBlockMaterial == null || lineBlockMaterial == Material.AIR) withlineblocks = false;
+
+            Particle.DustOptions dust = new Particle.DustOptions(Color.YELLOW, (float) EasyGuard.getPlugin().getConfig().getDouble("POINT_SIZE"));
 
             world.spawnParticle(Particle.REDSTONE, locat1, EasyGuard.getPlugin().getConfig().getInt("MASTER_POINT_PARTICLE_COUNT"), dust);
 
@@ -73,7 +82,7 @@ public class ClaimBoundsCommand implements CommandExecutor {
 
             world.spawnParticle(Particle.REDSTONE, locat2, EasyGuard.getPlugin().getConfig().getInt("MASTER_POINT_PARTICLE_COUNT"), dust);
 
-            // NOTE: Draw points - END DO NOT CHANGE
+
 
             dust = new Particle.DustOptions(Color.GRAY, (float) EasyGuard.getPlugin().getConfig().getDouble("LINE_SIZE"));
 
@@ -84,64 +93,181 @@ public class ClaimBoundsCommand implements CommandExecutor {
             this.drawLineBetweenParticles(
                     new Location(world, locat1.getX(), locat1.getY(), locat1.getZ()),
                     new Location(world, locat1.getX(), locat2.getY(), locat1.getZ()),
-                    BASE_SEED, world, dust, COUNT, RANGE);
+                    BASE_SEED, world, dust, COUNT, RANGE, withlineblocks, lineBlockMaterial, player);
 
             this.drawLineBetweenParticles(
                     new Location(world, locat1.getX(), locat1.getY(), locat1.getZ()),
                     new Location(world, locat2.getX(), locat1.getY(), locat1.getZ()),
-                    BASE_SEED, world, dust, COUNT, RANGE);
+                    BASE_SEED, world, dust, COUNT, RANGE, withlineblocks, lineBlockMaterial, player);
 
             this.drawLineBetweenParticles(
                     new Location(world, locat1.getX(), locat1.getY(), locat1.getZ()),
                     new Location(world, locat1.getX(), locat1.getY(), locat2.getZ()),
-                    BASE_SEED, world, dust, COUNT, RANGE);
+                    BASE_SEED, world, dust, COUNT, RANGE, withlineblocks, lineBlockMaterial, player);
 
 
             this.drawLineBetweenParticles(
                     new Location(world, locat1.getX(), locat2.getY(), locat1.getZ()),
                     new Location(world, locat2.getX(), locat2.getY(), locat1.getZ()),
-                    BASE_SEED, world, dust, COUNT, RANGE);
+                    BASE_SEED, world, dust, COUNT, RANGE, withlineblocks, lineBlockMaterial, player);
 
             this.drawLineBetweenParticles(
                     new Location(world, locat1.getX(), locat2.getY(), locat1.getZ()),
                     new Location(world, locat1.getX(), locat2.getY(), locat2.getZ()),
-                    BASE_SEED, world, dust, COUNT, RANGE);
+                    BASE_SEED, world, dust, COUNT, RANGE, withlineblocks, lineBlockMaterial, player);
 
             this.drawLineBetweenParticles(
                     new Location(world, locat2.getX(), locat1.getY(), locat2.getZ()),
                     new Location(world, locat2.getX(), locat2.getY(), locat2.getZ()),
-                    BASE_SEED, world, dust, COUNT, RANGE);
+                    BASE_SEED, world, dust, COUNT, RANGE, withlineblocks, lineBlockMaterial, player);
 
             this.drawLineBetweenParticles(
                     new Location(world, locat2.getX(), locat1.getY(), locat1.getZ()),
                     new Location(world, locat2.getX(), locat2.getY(), locat1.getZ()),
-                    BASE_SEED, world, dust, COUNT, RANGE);
+                    BASE_SEED, world, dust, COUNT, RANGE, withlineblocks, lineBlockMaterial, player);
 
             this.drawLineBetweenParticles(
                     new Location(world, locat1.getX(), locat1.getY(), locat2.getZ()),
                     new Location(world, locat1.getX(), locat2.getY(), locat2.getZ()),
-                    BASE_SEED, world, dust, COUNT, RANGE);
+                    BASE_SEED, world, dust, COUNT, RANGE, withlineblocks, lineBlockMaterial, player);
 
             this.drawLineBetweenParticles(
                     new Location(world, locat1.getX(), locat2.getY(), locat2.getZ()),
                     new Location(world, locat2.getX(), locat2.getY(), locat2.getZ()),
-                    BASE_SEED, world, dust, COUNT, RANGE);
+                    BASE_SEED, world, dust, COUNT, RANGE, withlineblocks, lineBlockMaterial, player);
 
             this.drawLineBetweenParticles(
                     new Location(world, locat2.getX(), locat2.getY(), locat1.getZ()),
                     new Location(world, locat2.getX(), locat2.getY(), locat2.getZ()),
-                    BASE_SEED, world, dust, COUNT, RANGE);
+                    BASE_SEED, world, dust, COUNT, RANGE, withlineblocks, lineBlockMaterial, player);
 
             this.drawLineBetweenParticles(
                     new Location(world, locat1.getX(), locat1.getY(), locat2.getZ()),
                     new Location(world, locat2.getX(), locat1.getY(), locat2.getZ()),
-                    BASE_SEED, world, dust, COUNT, RANGE);
+                    BASE_SEED, world, dust, COUNT, RANGE, withlineblocks, lineBlockMaterial, player);
 
             this.drawLineBetweenParticles(
                     new Location(world, locat2.getX(), locat1.getY(), locat1.getZ()),
                     new Location(world, locat2.getX(), locat1.getY(), locat2.getZ()),
-                    BASE_SEED, world, dust, COUNT, RANGE);
+                    BASE_SEED, world, dust, COUNT, RANGE, withlineblocks, lineBlockMaterial, player);
 
+
+            if (withcorrorblocks) {
+
+                player.sendBlockChange(locat1, corrorBlockMaterial.createBlockData());
+
+                player.sendBlockChange(new Location(player.getWorld(), locat2.getX(), locat1.getY(), locat2.getZ()), corrorBlockMaterial.createBlockData());
+                player.sendBlockChange(new Location(player.getWorld(), locat1.getX(), locat2.getY(), locat2.getZ()), corrorBlockMaterial.createBlockData());
+                player.sendBlockChange(new Location(player.getWorld(), locat2.getX(), locat2.getY(), locat1.getZ()), corrorBlockMaterial.createBlockData());
+                player.sendBlockChange(new Location(player.getWorld(), locat1.getX(), locat2.getY(), locat1.getZ()), corrorBlockMaterial.createBlockData());
+
+
+                player.sendBlockChange(new Location(player.getWorld(), locat2.getX(), locat2.getY(), locat2.getZ()), corrorBlockMaterial.createBlockData());
+                player.sendBlockChange(new Location(player.getWorld(), locat1.getX(), locat1.getY(), locat2.getZ()), corrorBlockMaterial.createBlockData());
+                player.sendBlockChange(new Location(player.getWorld(), locat2.getX(), locat1.getY(), locat1.getZ()), corrorBlockMaterial.createBlockData());
+                player.sendBlockChange(new Location(player.getWorld(), locat1.getX(), locat1.getY(), locat1.getZ()), corrorBlockMaterial.createBlockData());
+
+                player.sendBlockChange(locat2, corrorBlockMaterial.createBlockData());
+
+                new BukkitRunnable() {
+
+                    @Override
+                    public void run() {
+
+                        player.sendBlockChange(locat1, locat1.getBlock().getBlockData());
+
+                        player.sendBlockChange(new Location(player.getWorld(), locat2.getX(), locat1.getY(), locat2.getZ()), new Location(player.getWorld(), locat2.getX(), locat1.getY(), locat2.getZ()).getBlock().getBlockData());
+                        player.sendBlockChange(new Location(player.getWorld(), locat1.getX(), locat2.getY(), locat2.getZ()), new Location(player.getWorld(), locat1.getX(), locat2.getY(), locat2.getZ()).getBlock().getBlockData());
+                        player.sendBlockChange(new Location(player.getWorld(), locat2.getX(), locat2.getY(), locat1.getZ()), new Location(player.getWorld(), locat2.getX(), locat2.getY(), locat1.getZ()).getBlock().getBlockData());
+                        player.sendBlockChange(new Location(player.getWorld(), locat1.getX(), locat2.getY(), locat1.getZ()), new Location(player.getWorld(), locat1.getX(), locat2.getY(), locat1.getZ()).getBlock().getBlockData());
+
+
+                        player.sendBlockChange(new Location(player.getWorld(), locat2.getX(), locat2.getY(), locat2.getZ()), new Location(player.getWorld(), locat2.getX(), locat2.getY(), locat2.getZ()).getBlock().getBlockData());
+                        player.sendBlockChange(new Location(player.getWorld(), locat1.getX(), locat1.getY(), locat2.getZ()), new Location(player.getWorld(), locat1.getX(), locat1.getY(), locat2.getZ()).getBlock().getBlockData());
+                        player.sendBlockChange(new Location(player.getWorld(), locat2.getX(), locat1.getY(), locat1.getZ()), new Location(player.getWorld(), locat2.getX(), locat1.getY(), locat1.getZ()).getBlock().getBlockData());
+                        player.sendBlockChange(new Location(player.getWorld(), locat1.getX(), locat1.getY(), locat1.getZ()), new Location(player.getWorld(), locat1.getX(), locat1.getY(), locat1.getZ()).getBlock().getBlockData());
+
+                        player.sendBlockChange(locat2, locat2.getBlock().getBlockData());
+
+                    }
+
+                }.runTaskLater(EasyGuard.getPlugin(), EasyGuard.getPlugin().getConfig().getInt("SHOW_FOR_SECONDS") * 20L);
+
+            }
+
+            if (withlineblocks) {
+
+                new BukkitRunnable() {
+
+                    @Override
+                    public void run() {
+
+                        ClaimBoundsCommand.resetdrawLineBetweenParticles(
+                                new Location(world, locat1.getX(), locat1.getY(), locat1.getZ()),
+                                new Location(world, locat1.getX(), locat2.getY(), locat1.getZ()),
+                                BASE_SEED, world, RANGE, player);
+
+                        ClaimBoundsCommand.resetdrawLineBetweenParticles(
+                                new Location(world, locat1.getX(), locat1.getY(), locat1.getZ()),
+                                new Location(world, locat2.getX(), locat1.getY(), locat1.getZ()),
+                                BASE_SEED, world, RANGE, player);
+
+                        ClaimBoundsCommand.resetdrawLineBetweenParticles(
+                                new Location(world, locat1.getX(), locat1.getY(), locat1.getZ()),
+                                new Location(world, locat1.getX(), locat1.getY(), locat2.getZ()),
+                                BASE_SEED, world, RANGE, player);
+
+
+                        ClaimBoundsCommand.resetdrawLineBetweenParticles(
+                                new Location(world, locat1.getX(), locat2.getY(), locat1.getZ()),
+                                new Location(world, locat2.getX(), locat2.getY(), locat1.getZ()),
+                                BASE_SEED, world, RANGE, player);
+
+                        ClaimBoundsCommand.resetdrawLineBetweenParticles(
+                                new Location(world, locat1.getX(), locat2.getY(), locat1.getZ()),
+                                new Location(world, locat1.getX(), locat2.getY(), locat2.getZ()),
+                                BASE_SEED, world, RANGE, player);
+
+                        ClaimBoundsCommand.resetdrawLineBetweenParticles(
+                                new Location(world, locat2.getX(), locat1.getY(), locat2.getZ()),
+                                new Location(world, locat2.getX(), locat2.getY(), locat2.getZ()),
+                                BASE_SEED, world, RANGE, player);
+
+                        ClaimBoundsCommand.resetdrawLineBetweenParticles(
+                                new Location(world, locat2.getX(), locat1.getY(), locat1.getZ()),
+                                new Location(world, locat2.getX(), locat2.getY(), locat1.getZ()),
+                                BASE_SEED, world, RANGE, player);
+
+                        ClaimBoundsCommand.resetdrawLineBetweenParticles(
+                                new Location(world, locat1.getX(), locat1.getY(), locat2.getZ()),
+                                new Location(world, locat1.getX(), locat2.getY(), locat2.getZ()),
+                                BASE_SEED, world, RANGE, player);
+
+                        ClaimBoundsCommand.resetdrawLineBetweenParticles(
+                                new Location(world, locat1.getX(), locat2.getY(), locat2.getZ()),
+                                new Location(world, locat2.getX(), locat2.getY(), locat2.getZ()),
+                                BASE_SEED, world, RANGE, player);
+
+                        ClaimBoundsCommand.resetdrawLineBetweenParticles(
+                                new Location(world, locat2.getX(), locat2.getY(), locat1.getZ()),
+                                new Location(world, locat2.getX(), locat2.getY(), locat2.getZ()),
+                                BASE_SEED, world, RANGE, player);
+
+                        ClaimBoundsCommand.resetdrawLineBetweenParticles(
+                                new Location(world, locat1.getX(), locat1.getY(), locat2.getZ()),
+                                new Location(world, locat2.getX(), locat1.getY(), locat2.getZ()),
+                                BASE_SEED, world, RANGE, player);
+
+                        ClaimBoundsCommand.resetdrawLineBetweenParticles(
+                                new Location(world, locat2.getX(), locat1.getY(), locat1.getZ()),
+                                new Location(world, locat2.getX(), locat1.getY(), locat2.getZ()),
+                                BASE_SEED, world, RANGE, player);
+
+                    }
+
+                }.runTaskLater(EasyGuard.getPlugin(), EasyGuard.getPlugin().getConfig().getInt("SHOW_FOR_SECONDS") * 20L);
+
+            }
 
             sender.sendMessage(String.format("%sYou are currently in %s%s%s", ChatColor.BLUE, ChatColor.GOLD, ChatColor.BOLD, region.getId()));
 
@@ -157,7 +283,7 @@ public class ClaimBoundsCommand implements CommandExecutor {
 
     }
 
-    private void drawLineBetweenParticles(Location current, Location target, final double BASE_SPEED, World world, Particle.DustOptions dust, int count, double range) {
+    private void drawLineBetweenParticles(Location current, Location target, final double BASE_SPEED, World world, Particle.DustOptions dust, int count, double range, boolean withlineblocks, Material blockMaterial, Player player) {
 
         double diffX = (current.getX() - target.getX());
         double diffY = (current.getY() - target.getY());
@@ -178,6 +304,42 @@ public class ClaimBoundsCommand implements CommandExecutor {
         while (Math.abs(x - target.getX()) > range || Math.abs(y - target.getY()) > range || Math.abs(z - target.getZ()) > range) {
 
             world.spawnParticle(Particle.REDSTONE, new Location(world, x, y, z), count, dust);
+
+            if (withlineblocks) {
+
+                player.sendBlockChange(new Location(world, x, y, z), blockMaterial.createBlockData());
+
+            }
+
+            x += xv;
+            y += yv;
+            z += zv;
+
+        }
+
+    }
+
+    private static void resetdrawLineBetweenParticles(Location current, Location target, final double BASE_SPEED, World world, double range, Player player) {
+
+        double diffX = (current.getX() - target.getX());
+        double diffY = (current.getY() - target.getY());
+        double diffZ = (current.getZ() - target.getZ());
+
+        double distance = current.distance(target);
+
+        final double SPEED = -BASE_SPEED;
+
+        double xv = ((SPEED / distance) * diffX);
+        double yv = ((SPEED / distance) * diffY);
+        double zv = ((SPEED / distance) * diffZ);
+
+        double x = current.getX();
+        double y = current.getY();
+        double z = current.getZ();
+
+        while (Math.abs(x - target.getX()) > range || Math.abs(y - target.getY()) > range || Math.abs(z - target.getZ()) > range) {
+
+            player.sendBlockChange(new Location(world, x, y, z), new Location(world, x, y, z).getBlock().getBlockData());
 
             x += xv;
             y += yv;
